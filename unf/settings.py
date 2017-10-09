465836678e5ba19707c917e6ca5cb8bd08dfa5f2
Tyler Coverstone <coverstonet@gmail.com>
2017-10-08 23:26:58 -0400

    wget into docker

--- unf/settings.py
+++ unf/settings.py
@@ -73,26 +73,24 @@ WSGI_APPLICATION = 'unf.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 """
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myprojectdb',
-	'USER':'student',
-        'PASSWORD':'LetMeIn@UNF',
+        'NAME': 'SMIRK',
+	'USER':'root',
+        'PASSWORD':'appsec',
 	'HOST':'localhost',
 	'PORT':'',
     }
 }
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
