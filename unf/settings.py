677c2cf92d7b3a6353b19d6762f618f19cb75a70
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-10-16 20:42:16 -0400

    First version of application moved into github

--- unf/settings.py
+++ unf/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+SECRET_KEY = os.environ['SECRET_KEY']
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's4($v(n4g#4jdty$sj4+9gnc^n@td-gk_3mj6p0g^v0%)9p=0b'
+# SECRET_KEY = 's4($v(n4g#4jdty$sj4+9gnc^n@td-gk_3mj6p0g^v0%)9p=0b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
